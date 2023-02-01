0ae1bd9ed00bf959c210984ccf4d3cb1b9299d36
Alan Guilherme Bispo da Silva <alangui.1910@hotmail.com>
2023-01-31 23:01:24 -0300

    Feat: Add function where active or not active the button

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 // Som Pom
 function tocaSom(idElementAudio) {
-    document.querySelector(idElementAudio).play();
+   let elemento = document.querySelector(idElementAudio);
+    if (elemento === null) {
+        console.error('Elemento Não encontrado!');
+    }
+    if(elemento != null) {
+        if (elemento.localName === "audio") {
+            elemento.play(); 
+        }
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+for  (let contador = 0; contador < listaDeTeclas.length; contador+=1) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`
-    listaDeTeclas[contador].onclick = function () {
+    const idAudio = `#som_${instrumento}`;
+    
+
+    // Adicionando somente Enter e Espaço para serem utilizados para ativar o estilo do clique
+    tecla.onclick = function () {
         tocaSom(`${idAudio}`);
     }
-    contador += 1;
+
+    // Removendo o estilo do clique
+
+    tecla.onkeydown = function (e) {
+        if(e.code == "Enter" | "Space") {
+        tecla.classList.add('ativa');
+        }
+    }
+    
+    // Removendo a classe após soltar a tecla
+    
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
